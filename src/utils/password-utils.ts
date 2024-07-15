fad846159b22688ad52d6fbc28009a8de7c3e7a3
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2024-07-15 18:38:32 -0300

    chore: Update .env.example, .gitignore, and next.config.ts

---
+++ src/utils/password-utils.ts
@@ -0,0 +1,9 @@
+// TODO: Implement password hashing and comparison functions
+
+export function hashPassword (password: string) {
+  return password
+}
+
+export function comparePassword (password: string, hashedPassword: string) {
+  return password === hashedPassword
+}
