ef77704041cbc77ab9265934d15dd78320603999
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2024-07-16 23:48:07 -0300

    chore: Add Settings layout

---
+++ src/app/(settings)/settings/general/page.tsx
@@ -0,0 +1,59 @@
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'General Settings',
+  description: 'Dashboard'
+}
+
+export default function General () {
+  return (
+    <>
+      <div className="mx-auto grid w-full max-w-6xl gap-2">
+        <h1 className="text-3xl font-semibold">Settings</h1>
+      </div>
+      <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+        <nav
+          className="grid gap-4 text-sm text-zinc-400" x-chunk="dashboard-04-chunk-0"
+        >
+          <Link href="#" className="font-semibold text-primary">
+            General
+          </Link>
+          <Link href="#">Security</Link>
+          <Link href="#">Integrations</Link>
+          <Link href="#">Support</Link>
+          <Link href="#">Organizations</Link>
+          <Link href="#">Advanced</Link>
+        </nav>
+        <div className="grid gap-6">
+          <Card x-chunk="dashboard-04-chunk-1">
+            <CardHeader>
+              <CardTitle>Store Name</CardTitle>
+              <CardDescription>
+                Used to identify your store in the marketplace.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <Input placeholder="Store Name" />
+              </form>
+            </CardContent>
+            <CardFooter className="border-t px-6 py-4 border-zinc-800">
+              <Button>Save</Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/app/(settings)/settings/layout.tsx
@@ -0,0 +1,53 @@
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { CircleUser, Package2 } from 'lucide-react'
+import Link from 'next/link'
+
+export default function RootLayout ({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <header className="sticky top-0 flex h-16 items-center gap-4 border-b dark:border-zinc-800 bg-zinc-950 px-4 md:px-6">
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+          >
+            <Package2 className="h-6 w-6" />
+            <span className="sr-only">Split Group</span>
+          </Link>
+        </nav>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full ml-auto">
+              <CircleUser className="h-5 w-5" />
+              <span className="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </header>
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+        {children}
+      </main>
+    </div>
+  )
+}
