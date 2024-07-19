adae7aba1a8086f3d2a947a822da9d33c532090c
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2024-07-18 23:54:21 -0300

    feat: Add UserNav component for user navigation menu

---
+++ src/components/user-nav.tsx
@@ -0,0 +1,44 @@
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Button } from './ui/button'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { SignOutButton } from './sign-out-button'
+
+export const UserNav = async ({ className }: { className?: string }) => {
+  const session = await getServerSession(authOptions)
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="secondary" size="icon" className={cn('rounded-full', className)}>
+          <span>
+            {session?.user?.name[0].toUpperCase()}
+          </span>
+          <span className="sr-only">Alternar menú de usuario</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Mi cuenta</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href="/settings">
+            Configuración
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <SignOutButton />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
