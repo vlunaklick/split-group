d8918ed7077a5bf3f16007a837443cca9e645670
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2024-08-17 22:42:35 -0300

    refactor: Update SpendInfo component to use Skeleton for loading state

--- src/app/(overview)/groups/[groupId]/spendings/[spendId]/info.tsx
+++ src/app/(overview)/groups/[groupId]/spendings/[spendId]/info.tsx
@@ -4,11 +4,12 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { useGetSpendingById } from '@/data/spendings'
 import { formatDate } from '@/lib/dates'
 import { formatMoney } from '@/lib/money'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export const SpendInfo = ({ groupId, spendId, userId }: { groupId: string, spendId: string, userId: string }) => {
   const { data, isLoading } = useGetSpendingById({ spendingId: spendId })
 
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <SpendInfoSkeleton />
   if (!data) return <div>Failed to load</div>
 
   return (
@@ -26,7 +27,7 @@ export const SpendInfo = ({ groupId, spendId, userId }: { groupId: string, spend
         </div>
 
         <div>
-          <p className='text-muted-foreground/80'>Descripción:</p>
+          <p className='text-muted-foreground/80 text-xs'>Descripción:</p>
           <p>{data.description}</p>
         </div>
 
@@ -55,6 +56,56 @@ export const SpendInfo = ({ groupId, spendId, userId }: { groupId: string, spend
             <p>{data.currency.name}</p>
           </div>
         </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+const SpendInfoSkeleton = () => {
+  return (
+    <Card className='w-full'>
+      <CardHeader>
+        <CardTitle>Gasto</CardTitle>
+        <CardDescription>
+          Información general del gasto
+        </CardDescription>
+      </CardHeader>
+      <CardContent className='space-y-4 text-sm'>
+        <div>
+          <p className='text-muted-foreground/80 text-xs'>Nombre:</p>
+          <Skeleton className='w-36 h-5' />
+        </div>
+
+        <div>
+          <p className='text-muted-foreground/80 text-xs'>Descripción:</p>
+          <Skeleton className='w-64 h-5' />
+        </div>
+
+        <div>
+          <p className='text-muted-foreground/80 text-xs'>Creado por:</p>
+          <Skeleton className='w-24 h-5' />
+        </div>
+
+        <div>
+          <p className='text-muted-foreground/80 text-xs'>Categoría:</p>
+          <Skeleton className='w-24 h-5' />
+        </div>
+
+        <div>
+          <p className='text-muted-foreground/80 text-xs'>Fecha:</p>
+          <Skeleton className='w-24 h-5' />
+        </div>
+
+        <div className='flex gap-4 justify-between'>
+          <div>
+            <p className='text-muted-foreground/80 text-xs'>Monto:</p>
+            <Skeleton className='w-24 h-5' />
+          </div>
+          <div>
+            <p className='text-muted-foreground/80 text-xs'>Moneda:</p>
+            <Skeleton className='w-12 h-5' />
+          </div>
+          </div>
       </CardContent>
     </Card>
   )
