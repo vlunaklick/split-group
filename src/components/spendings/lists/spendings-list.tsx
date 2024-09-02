f908cca4cd0d1656f4d17cc11132d154343e3fce
vlunaklick <v.lunaklick@gmail.com>
2024-09-01 22:57:11 -0300

    feat: Add skeleton spending list

--- src/components/spendings/lists/spendings-list.tsx
+++ src/components/spendings/lists/spendings-list.tsx
@@ -4,9 +4,10 @@ import { useGetSpendingsTable } from '@/data/spendings'
 import { MobileSpendingTable, SpendingTable } from '../tables/spending-table'
 import { GetSpendingsSchema } from '@/lib/validations'
 import { useEffect } from 'react'
+import { DataTableSkeleton } from '@/components/data-table/data-table-skeleton'
 
 export const SpendingsList = ({ groupId, searchParams }: { groupId: string, searchParams: GetSpendingsSchema }) => {
-  const { data, error, mutate } = useGetSpendingsTable({ groupId, searchParams })
+  const { data, error, mutate, isLoading } = useGetSpendingsTable({ groupId, searchParams })
 
   useEffect(() => {
     mutate(['spendings-table', groupId])
@@ -17,8 +18,25 @@ export const SpendingsList = ({ groupId, searchParams }: { groupId: string, sear
 
   return (
     <>
-      <SpendingTable data={data} groupId={groupId}/>
-      <MobileSpendingTable data={data?.data ?? []} groupId={groupId} />
+      {isLoading && (
+        <DataTableSkeleton
+          columnCount={5}
+          rowCount={10}
+          searchableColumnCount={1}
+          showViewOptions
+          cellWidths={['auto', 'auto', 'auto', 'auto', 'auto']}
+          withPagination
+          shrinkZero
+        />
+      )}
+      {
+        !isLoading && (
+          <>
+            <SpendingTable data={data} groupId={groupId}/>
+            <MobileSpendingTable data={data?.data ?? []} groupId={groupId} />
+          </>
+        )
+      }
     </>
   )
 }
