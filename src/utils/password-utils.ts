d96e48a33e0cb0183123c8354b09406b50c20bd1
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2024-09-25 11:42:09 -0300

    Refactor: Update password-utils to use bcrypt for hashing and comparing passwords

--- src/utils/password-utils.ts
+++ src/utils/password-utils.ts
@@ -1,14 +1,11 @@
-// TODO: Cambiar cuando se lleve completamente a prod
 // @ts-ignore
-// import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt'
 
 export function hashPassword (password: string) {
-  return password
-  // const salt = bcrypt.genSaltSync(10)
-  // return bcrypt.hashSync(password, salt)
+  const salt = bcrypt.genSaltSync(10)
+  return bcrypt.hashSync(password, salt)
 }
 
 export function comparePassword (password: string, hashedPassword: string) {
-  return password === hashedPassword
-  // return bcrypt.compareSync(password, hashedPassword)
+  return bcrypt.compareSync(password, hashedPassword)
 }
